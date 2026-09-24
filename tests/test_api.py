15b0597f7ebd4442e4b3875d1a943a143d9ba9b1
Troy N. Smith <troy.smith@uga.edu>
2026-09-24 14:49:26 -0600

    Add GCM abstraction

--- tests/test_api.py
+++ tests/test_api.py
@@ -271,7 +271,6 @@ class FuelLibAPIContractTestCase(unittest.TestCase):
             "Y_0",
             "Nij",
             "num_compounds",
-            "num_groups",
             "MW",
             "Tc",
             "Pc",
